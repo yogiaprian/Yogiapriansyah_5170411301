312cd4d1cb2d0a7cea3bf7987bb9ed106b349b98
yogiaprian <44409232+yogiaprian@users.noreply.github.com>
2018-10-23 23:16:28 +0700

    Add files via upload

---
+++ pbo_yogiapriansyah_5170411301/src/yogiapriansyah_5170411301.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class yogiapriansyah_5170411301 {
+    public static void main(String[]args){
+
+int pil;
+Scanner s = new Scanner (System.in);
+
+for (pil=1; pil<=3;pil++)
+{
+    System.out.println("1. Input data ");
+    System.out.println("2. Tampil data ");
+    System.out.println("3. Keluar ");
+    System.out.print("Pilihan: "); pil = s.nextInt();
+}
+        
+}
+}
